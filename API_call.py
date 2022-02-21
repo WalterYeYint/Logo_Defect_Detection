05f6edd316031244597f7c7473ed1464a930f575
Kan Yu <WalterYeYint@gmail.com>
2022-02-21 12:13:33 +0630

    fix: change date format in API_call.py

--- API_call.py
+++ API_call.py
@@ -1,4 +1,3 @@
-import xxlimited
 import cv2, requests
 import os
 from datetime import datetime
@@ -43,7 +42,8 @@ def upload_to_server(frame, output_img, serial_no, result, server_url,save_faile
     cv2.imwrite('temp/output_img.jpg',output_img)
     file = {'image':open('temp/img.jpg','rb'), 'result_image':open('temp/output_img.jpg','rb')}
     x = datetime.now()
-    date = x.strftime("%Y")+' '+x.strftime("%m")+' '+x.strftime("%d")+'-'+x.strftime("%H")+':'+x.strftime("%M")+':'+x.strftime("%S")
+    date = x.strftime("%Y")+'-'+x.strftime("%m")+'-'+x.strftime("%d")+' '+x.strftime("%H")+':'+x.strftime("%M")+':'+x.strftime("%S")
+    date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
     data = {"serial_no": serial_no,
             "time": date, 
             "result": result
